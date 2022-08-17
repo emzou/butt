602437aa675186cf23d0faf4f675d6ded2c3859d
madgesaur <107163124+madgesaur@users.noreply.github.com>
2022-08-17 13:18:16 -0500

    Update make_bashscript.py

--- make_bashscript.py
+++ make_bashscript.py
@@ -1,25 +1,24 @@
 from pathlib import Path
+import pandas as pd 
 import glob, os
 from datetime import datetime
 
 # specify the subreddit you care about
 subreddit = "buttcoin"
 
-# figure out when the subreddit 
-# make sure to update this accordingly
-start_m = 7
-start_y = 2011
-
-end_m = 7
-end_y = 2022
-
 # TODO use datetime to get a list of all of the month and years between our start and end dates
 
-list_of_year_month_combos = [] # (2011, 7), (2011, 8), (2011, 9) ... (2022, 7)
+list_of_years = pd.date_range('2011-07-01','2012-07-31', freq='MS').strftime("%Y").tolist()
+list_of_months = pd.date_range('2011-07-01','2012-07-31', freq='MS').strftime("%m").tolist()
+lst_tuple = list (zip(list_of_years,list_of_months))
 
-for year_month in list_of_year_month_combos:
+for year_month in lst_tuple:
     year = year_month[0]
+    year = int (year)
     month = year_month[1]
+    month = int (month)
+    with open ("run_api_calls.sh", "a") as o: 
+        o.write (f"/Users/emilyzou/butt/unlucky.py -m {month} -y {year} -s buttcoin -o ./pushshift_data \n")
 
-    print("python unlucky.py -m {} -y {} -s {} -o {}")#TODO arg for printing to file
+    
     
