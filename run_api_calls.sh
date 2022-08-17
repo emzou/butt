29487e56985bca1c88b9cbe3e98935f0a0954fd9
madgesaur <107163124+madgesaur@users.noreply.github.com>
2022-08-17 13:18:33 -0500

    Update run_api_calls.sh

--- run_api_calls.sh
+++ run_api_calls.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+/Users/emilyzou/butt/unlucky.py -m 7 -y 2011 -s buttcoin -o ./pushshift_data 
+/Users/emilyzou/butt/unlucky.py -m 8 -y 2011 -s buttcoin -o ./pushshift_data 
+/Users/emilyzou/butt/unlucky.py -m 9 -y 2011 -s buttcoin -o ./pushshift_data 
+/Users/emilyzou/butt/unlucky.py -m 10 -y 2011 -s buttcoin -o ./pushshift_data 
+/Users/emilyzou/butt/unlucky.py -m 11 -y 2011 -s buttcoin -o ./pushshift_data 
+/Users/emilyzou/butt/unlucky.py -m 12 -y 2011 -s buttcoin -o ./pushshift_data 
+/Users/emilyzou/butt/unlucky.py -m 1 -y 2012 -s buttcoin -o ./pushshift_data 
+/Users/emilyzou/butt/unlucky.py -m 2 -y 2012 -s buttcoin -o ./pushshift_data 
+/Users/emilyzou/butt/unlucky.py -m 3 -y 2012 -s buttcoin -o ./pushshift_data 
+/Users/emilyzou/butt/unlucky.py -m 4 -y 2012 -s buttcoin -o ./pushshift_data 
+/Users/emilyzou/butt/unlucky.py -m 5 -y 2012 -s buttcoin -o ./pushshift_data 
+/Users/emilyzou/butt/unlucky.py -m 6 -y 2012 -s buttcoin -o ./pushshift_data 
+/Users/emilyzou/butt/unlucky.py -m 7 -y 2012 -s buttcoin -o ./pushshift_data 
